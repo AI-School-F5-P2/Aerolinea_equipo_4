6c7a44806a903ec583cc36bf00618215a20e5ef4
Gala1812 Chupirova <galynachupiryova@gmail.com>
2023-09-06 13:17:03 +0000

    feat: prediction function integrated in predict

--- predict_page.py
+++ predict_page.py
@@ -18,8 +18,8 @@ scaler = my_pipeline["scaler"]
 def show_predict_page():
     st.title("Formulario satisfacción de clientes")
 
-    st.write("""### Por favor complete nuestra encuesta para evaluar la calidad de nuestros servicios. 
-                Le llevará un máximo de 10 minutos y nos ayudará a mejorar la calidad de los servicios que brindamos.""")
+    st.write("""### Por favor complete nuestra encuesta para evaluar la calidad de nuestros servicios.""")
+    st.subheader("Le llevará un máximo de 10 minutos y nos ayudará a mejorar la calidad de los servicios que brindamos.")
 
 
     Gender  = (
@@ -49,7 +49,7 @@ def show_predict_page():
     Age = st.slider("Edad", 0, 100, 1)
     Flight_distance = st.slider("Distancia de vuelo", 0, 5000, 10)
     Inflight_wifi_service = st.slider("Servicio wifi a bordo", 0, 5, 1)
-    Departure_Arrival_time_convenien = st.slider("Hora de salida/llegada conveniente", 0, 5, 1)
+    Departure_Arrival_time_convenient = st.slider("Hora de salida/llegada conveniente", 0, 5, 1)
     Ease_of_Online_booking = st.slider("Facilidad de reserva en línea", 0, 5, 1)
     Gate_location = st.slider("Ubicación de la puerta", 0, 5, 1)
     Food_and_drink = st.slider("Comida y bebida", 0, 5, 1)
@@ -60,22 +60,20 @@ def show_predict_page():
     Leg_room_service = st.slider("Servicio de habitaciones para piernas", 0, 5, 1)
     Baggage_handling = st.slider("Manejo de equipaje", 0, 5, 1)
     Checkin_service = st.slider("Servicio de Check-in", 0, 5, 1)
-    Inflight_service = st.slider("Servicio a bordo", 0, 5, 1)
-    Cleanliness = st.slider("Limpieza", 0, 5, 1)
+    Inflight_service = st.slider("Servicio a bordo", 0, 5, 1, key="inflight_service_slider")
+    Cleanliness = st.slider("Limpieza", 0, 5, 1, key="Cleanliness_slider")
     Departure_delay_minutes = st.slider("Retraso de salida en minutos", 0, 1600, 1)
-    Arrival_delay_in_minutes = st.slider("Retraso de llegada en minutos", 0, 1600, 1)
+    Arrival_delay_minutes = st.slider("Retraso de llegada en minutos", 0, 1600, 1)
 
 
 
-
-    
-
-    #ok = st.button("Calculate Salary")
-    #if ok:
-        #X = np.array([[country, education, expericence ]])
-        #X[:, 0] = le_country.transform(X[:,0])
-        #X[:, 1] = le_education.transform(X[:,1])
+    ok = st.button("Nivel de satisfacción")
+    if ok:
+        X = np.array([[Gender, Customer_Type, Type_of_Travel, Class, Inflight_wifi_service, Departure_Arrival_time_convenient, Ease_of_Online_booking, Gate_location, Food_and_drink, Online_boarding, Seat_comfort, Inflight_entertainment, Onboard_service, Leg_room_service, Baggage_handling, Checkin_service, Inflight_service, Cleanliness, Age, Flight_distance, Departure_delay_minutes, Arrival_delay_minutes]])
+        #["Gender", "Customer Type", 'Type of Travel', 'Class', "Inflight wifi service", "Departure/Arrival time convenient", 'Ease of Online booking', 'Gate location', 'Food and drink', 'Online boarding', 'Seat comfort', 'Inflight entertainment', 'On-board service', 'Leg room service', 'Baggage handling', 'Checkin service', 'Inflight service', 'Cleanliness']
+        X[:,0:18] = OH_enc.transform(X[:,0:18])
+        X[:, 19:22] = scaler.transform(X[:,19:22])
         #X = X.astype(float)
 
-        #salary = regressor.predict(X)
-        #st.subheader(f"The estimated salary is ${salary[0]:.2f}")
+        Nivel_de_satisfacción = model.predict(X)
+        st.subheader(f"Nivel de satisfacción es ${Nivel_de_satisfacción}")
